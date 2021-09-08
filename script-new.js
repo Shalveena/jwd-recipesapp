fd2661b182251494a99e4419141430243544ce62
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-09-08 16:17:37 +1000

    Start JavaScript

--- script-new.js
+++ script-new.js
@@ -1,9 +1,22 @@
 "use strict";
 
-// DOM Elements
+// ---- DOM Elements --------------------------------------------------
 
-document.querySelector();
+const newRecipeBtn = document.querySelector("#new-recipe-btn");
+const modal = document.querySelector("#modal");
+const overlay = document.querySelector("#overlay");
 
-// Function
+// ---- Function ------------------------------------------------------
 
-// Event Handlers
+// Show Modal
+// const showModal = () => {
+//   const hiddenElements = document.getElementsByClassName("display-none");
+//   for (let i = 0; i < hiddenElements.length; i++) {
+//     hiddenElements[i].classList.remove("display-none");
+//   }
+// };
+
+// ---- Event Handlers ------------------------------------------------
+
+// Event listener for Add New Recipe Button
+newRecipeBtn.addEventListener("click", (e) => );
