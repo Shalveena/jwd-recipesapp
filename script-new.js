3dbfb1e93878d0366c3a561fa000b027c5b0100b
Shalveena Rohde <shalveena.s.prasad@gmail.com>
2021-09-08 16:41:19 +1000

    Event listeners to open and close modal

--- script-new.js
+++ script-new.js
@@ -3,20 +3,31 @@
 // ---- DOM Elements --------------------------------------------------
 
 const newRecipeBtn = document.querySelector("#new-recipe-btn");
-const modal = document.querySelector("#modal");
+const modal = document.querySelector(".form-container");
 const overlay = document.querySelector("#overlay");
+// const closeModalBtn = doucment.querySelector("#close-modal");
 
 // ---- Function ------------------------------------------------------
 
-// Show Modal
-// const showModal = () => {
-//   const hiddenElements = document.getElementsByClassName("display-none");
-//   for (let i = 0; i < hiddenElements.length; i++) {
-//     hiddenElements[i].classList.remove("display-none");
-//   }
-// };
+// Open Modal
+const openModal = () => {
+  modal.classList.remove("display-none");
+  overlay.classList.remove("display-none");
+};
 
+// Close Modal
+
+const closeModal = () => {
+  modal.classList.add("display-none");
+  overlay.classList.add("display-none");
+};
 // ---- Event Handlers ------------------------------------------------
 
-// Event listener for Add New Recipe Button
-newRecipeBtn.addEventListener("click", (e) => );
+// Event listener for Add New Recipe Button (to open modal)
+newRecipeBtn.addEventListener("click", openModal);
+
+// Event listener to X button (to close modal)
+// closeModalBtn.addEventListener("click", closeModal);
+
+// Event listener to overlay (to hide it)
+overlay.addEventListener("click", closeModal);
